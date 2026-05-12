a59eed355e1da86f3ac7d85c6521bdbad66ccdef
davitkhanal <khanaldavit@gmail.com>
2026-05-12 23:40:51 +0545

    fix: update build scripts and Vite configuration for improved deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-import { cloudflare } from "@cloudflare/vite-plugin";
+import { cloudflare } from '@cloudflare/vite-plugin';
+
+const base = process.env.VITE_BASE || '/';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), cloudflare()],
-  base: '/Portfolio2026/',
+  base,
 });
